e8d4a2496bd4ed8e75d37166d54cb165942871ec
Abhimanyu Rana <abhimanyurana@192.168.1.6>
2023-01-09 21:25:47 +0530

    Added html template and made them dynamic

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render
-
+from datetime import date
 # Create your views here.
 
+
+all_posts = [
+    {
+        'slug':'hike-in-the-mountains',
+        'image':'mountains.jpg',
+        'author':'Tom',
+        'date':date(2023,1,6),
+        'title':'Mountain Hiking',
+        'excerpt':'some text',
+        'content':"""
+            lorem ipsum text comes to the picture
+            lorem ipsum text comes to the picture
+            lorem ipsum text comes to the picture
+            lorem ipsum text comes to the picture
+            lorem ipsum text comes to the picture
+            lorem ipsum text comes to the picture
+            lorem ipsum text comes to the picture
+        """
+    }
+]
+
+def get_date(post):
+    return post['date']
+
 def starting_page(request):
-    return render(request,"blog/index.html")
+    # sorted_posts = all_posts.sort(key=get_date)
+    sorted_posts = sorted(all_posts,key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request,"blog/index.html",{
+        "posts":latest_posts
+    })
 
 def posts(request):
-    pass
+    return render(request,"blog/all-posts.html",{
+        "all_posts":all_posts
+    })
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html",{
+        "post":identified_post
+    })
